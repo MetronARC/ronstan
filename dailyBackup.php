f5542b2d09163ef42df808986d80c8dacb9b22d7
MetronARC <ricoroenaldo@metronarc.com>
2024-10-30 11:44:15 +0700

    fix cron job things

--- dailyBackup.php
+++ dailyBackup.php
@@ -2,10 +2,10 @@
 $database = "u558841402_ronstandb";
 $user = "u558841402_ronstan";
 $password = "2468g0a7A7B7*";
-$backup_file = "/backup/" . $database . "_" . date("Y-m-d_H-i-s") . ".sql";
+$backup_file = "/home/u558841402/public_html/backup/" . $database . "_" . date("Y-m-d_H-i-s") . ".sql";
 
 // Run the mysqldump command to back up the database
-$command = "mysqldump --user=$user --password=$password $database > $backup_file";
+$command = "/usr/bin/mysqldump --user=$user --password=$password $database > $backup_file";
 system($command, $output);
 
 if ($output === 0) {
