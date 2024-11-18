da1b99c75d18a1d98f04bd5c9a4400b8670ffe46
MetronARC <ricoroenaldo@metronarc.com>
2024-11-18 10:04:26 +0700

    switching off REST

--- Dashboard/recap.php
+++ Dashboard/recap.php
@@ -285,6 +285,8 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script src="js/index.js"></script>
+
+    
     <script>
         let individualChartInstance = null;
 
